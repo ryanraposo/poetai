a7735265d692d2a8162ae2fdc42910ac3766b879
RickEvry <rickevry@gmail.com>
2023-07-23 12:27:05 +0200

    work

--- main.py
+++ main.py
@@ -37,9 +37,7 @@ if __name__ == '__maxxxxxxxin__':
 
 @app.route("/")
 def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
