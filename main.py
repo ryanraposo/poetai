d898fc02bb074dc05b81bbd483d8383b7e3343c0
RickEvry <rickevry@gmail.com>
2023-07-23 19:04:47 +0200

    work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 from hand import Hand
 from flask import Flask, request
-import random
-import string
 
 app = Flask(__name__)
 
@@ -17,15 +15,12 @@ def hello_world():
 
     lines = text1.split('|')
     
-    biases = [.75 for i in lines]
+    biases = [(int(bias1) / 100) for i in lines]
     styles = [int(style1) for i in lines]
     stroke_colors = ['black' for i in lines]
     stroke_widths = [1 for i in lines]
 
-    tmpFilename = ''.join(random.choice(string.ascii_lowercase) for i in range(40))
-
     svgText = hand.write(
-        filename='img/'+tmpFilename+'.svg',
         lines=lines,
         biases=biases,
         styles=styles,
@@ -38,7 +33,7 @@ def hello_world():
 
     del hand
 
-    return "ver13 " + svgText
+    return "ver15 " + svgText
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
